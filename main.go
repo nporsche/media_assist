6bdec08ea46d58bd1830821fc8ebcdaeb8a1bb23
hangchen <hangchen@didichuxing.com>
2017-03-22 18:50:13 +0800

    impl

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+
+	"flag"
+	"github.com/rwcarlsen/goexif/exif"
+	"github.com/rwcarlsen/goexif/mknote"
+)
+
+var (
+	spath       string
+	dpath       string
+	filePattern string
+)
+
+func main() {
+	flag.StringVar(&spath, "spath", "./", "search source path")
+	flag.StringVar(&dpath, "dpath", "./", "destination path")
+	flag.StringVar(&filePattern, "file", "*", "file pattern")
+	flag.Parse()
+
+	filepath.Walk(spath, visit)
+}
+
+func visit(path string, f os.FileInfo, err error) error {
+	if f.IsDir() {
+		return nil
+	}
+	fileName := f.Name()
+	fileFullName := path
+	if ok, err := filepath.Match(filePattern, fileName); !ok || err != nil {
+		log.Println("WARNING: not match pattern", f.Name())
+		return nil
+	}
+	tm, err := getExifDatetime(fileFullName)
+	if err != nil {
+		log.Printf("WARNING: cannot get exif info from %s use mod time instead\n", fileFullName)
+		tm = f.ModTime()
+	}
+	destinateDir := filepath.Join(dpath, fmt.Sprintf("%4d", tm.Year()), fmt.Sprintf("%02d", int(tm.Month())))
+
+	_, err = exec.Command("mkdir", "-p", destinateDir).Output()
+	if err != nil {
+		log.Printf("FAILED: mkdir -p %s\n", destinateDir, err)
+		return nil
+	}
+
+	var targetFileName string
+	var targetFileFullName string
+
+	for i := 0; ; i++ {
+		if _, err := os.Stat(fileFullName); err == nil {
+			if i == 0 {
+				targetFileName = tm.Format("2006-01-02 150405") + filepath.Ext(f.Name())
+			} else {
+				targetFileName = fmt.Sprintf("%s(%d)%s", tm.Format("2006-01-02 150405"), i, filepath.Ext(f.Name()))
+			}
+			targetFileFullName = filepath.Join(destinateDir, targetFileName)
+			_, err = exec.Command("mv", "-n", fileFullName, targetFileFullName).Output()
+			if err != nil {
+				log.Printf("FAILED: mv -n %s %s\n", fileFullName, targetFileFullName)
+				break
+			}
+		} else {
+			break
+		}
+	}
+	log.Printf("SUCCESS: mv -n %s %s\n", fileFullName, targetFileFullName)
+
+	return nil
+}
+
+func getExifDatetime(fname string) (tm time.Time, err error) {
+	f, err := os.Open(fname)
+	if err != nil {
+		return tm, err
+	}
+
+	// Optionally register camera makenote data parsing - currently Nikon and
+	// Canon are supported.
+	exif.RegisterParsers(mknote.All...)
+
+	x, err := exif.Decode(f)
+	if err != nil {
+		return tm, err
+	}
+
+	// Two convenience functions exist for date/time taken and GPS coords:
+	return x.DateTime()
+}
